046fc9091ca0ba5748f1b83f3df739ea0f05d9c6
tuisemo <tuisemo@sina.cn>
2021-10-10 18:28:50 +0800

    create a base rollup pack config

---
+++ src/main.js
@@ -0,0 +1,19 @@
+class SandBoxWindow {
+  constructor(context, frameWindow) {
+    return new Proxy(frameWindow, {
+      get(target, name) {
+        if (name in context) {
+          return context[name]
+        }
+        return target[name]
+      },
+      set(target, name, value) {
+        if (name in context) {
+          return (context[name] = value)
+        }
+        target[name] = value
+      }
+    })
+  }
+}
+export default SandBoxWindow
